c16d72f8564dc31bc65926fe74ff6fcfeb838b9a
vshishanovart-star <vshishanov.art@gmail.com>
2026-06-11 17:46:54 +0300

    Add tool registry

---
+++ tool_registry.py
@@ -0,0 +1,26 @@
+TOOLS = {
+    "chat": {
+        "type": "script",
+        "target": "ai_memory_chat.py"
+    },
+
+    "file_analyzer": {
+        "type": "script",
+        "target": "read_file_ai.py"
+    },
+
+    "project_overview": {
+        "type": "script",
+        "target": "project_overview_ai.py"
+    },
+
+    "image_generation": {
+        "type": "comfyui",
+        "target": "open_comfyui"
+    },
+
+    "tts": {
+        "type": "tts",
+        "target": "open_qwen_tts"
+    }
+}
