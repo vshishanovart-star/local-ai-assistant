19318af489af471cc726d498fcce3048fcdba406
vshishanovart-star <vshishanov.art@gmail.com>
2026-06-13 23:24:15 +0300

    Add desktop launcher and custom application icons

--- tools/png_to_ico.py
+++ tools/png_to_ico.py
@@ -1,20 +1,37 @@
+from pathlib import Path
+
 from PIL import Image
 
-img = Image.open(
-    "assets/local_ai_icon.png"
+
+png_files = list(
+    Path("assets").glob("*.png")
 )
 
-img.save(
-    "assets/local_ai_icon.ico",
-    format="ICO",
-    sizes=[
-        (16, 16),
-        (32, 32),
-        (48, 48),
-        (64, 64),
-        (128, 128),
-        (256, 256)
-    ]
-)
+for png_file in png_files:
 
-print("ICO created.")
+    ico_file = png_file.with_suffix(
+        ".ico"
+    )
+
+    img = Image.open(
+        png_file
+    )
+
+    img.save(
+        ico_file,
+        format="ICO",
+        sizes=[
+            (16, 16),
+            (32, 32),
+            (48, 48),
+            (64, 64),
+            (128, 128),
+            (256, 256)
+        ]
+    )
+
+    print(
+        f"Created: {ico_file.name}"
+    )
+
+print("\nDone.")
